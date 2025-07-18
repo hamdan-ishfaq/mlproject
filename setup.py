e696476c8fdb42a260aaf6b250bfbb38b50216e4
hamdan-ishfaq <mhamdanishfaq@gmai.com>
2025-07-18 20:53:32 +0500

    Model Trainer

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import find_packages,setup
+from typing import List
 
+hyphen_e_dot = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''this func will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    return requirements
+    
+    
 setup(
 name='mlproject',
 version='0.0.1',
 author='Batman',
-author_email='mhamdanishfaq@gmail.com',
+author_email='legendhunter005@gmail.com',
 packages=find_packages(),
-install_requires=['pandas','numpy','seaborn']
+install_requires=get_requirements("requirements.txt")
     
 )
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,6 @@
 import os 
+os.environ["LOKY_MAX_CPU_COUNT"] = "4"
+
 import sys
 from src.exception import CustomException
 from src.logger import logging
@@ -9,6 +11,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -50,5 +55,7 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data,test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
