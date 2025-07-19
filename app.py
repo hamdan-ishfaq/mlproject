0d24f6ecffd4c87e7fae4bce39897b6d5061f8e4
hamdan-ishfaq <mhamdanishfaq@gmai.com>
2025-07-19 23:49:42 +0500

    basic frontend

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-
+import logging
+logging.basicConfig(level=logging.INFO)
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 application = Flask(__name__)
@@ -13,9 +14,10 @@ app = application
 def index():
     return render_template("index.html")
 
+
+
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    
     if request.method == 'GET':
         return render_template('home.html')
     else:
@@ -29,12 +31,13 @@ def predict_datapoint():
             writing_score=request.form.get("writing_score")
         )
         pred_df = data.get_data_as_data_frame()
-        print(pred_df)
-        
+        logging.info(f"Received input data:\n{pred_df}")
+
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        
-        return render_template("home.html",results=results[0])
+
+        return render_template("home.html", results=results[0])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
